f33429ee5f5f8cc6e6375fb01b7946d6c7d0385d
bruno <bruno.rtools@outlook.com>
2017-02-13 16:19:18 -0200

    Pasta vendor
    removi o ignore da pasta vendor

--- config/routes.php
+++ config/routes.php
@@ -43,6 +43,13 @@ use Cake\Routing\Route\DashedRoute;
  */
 Router::defaultRouteClass(DashedRoute::class);
 
+Router::prefix('admin', function ($routes) {
+    // All routes here will be prefixed with `/admin`
+    // And have the prefix => admin route element added.
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'login']);
+    $routes->fallbacks(DashedRoute::class);
+});
+
 Router::scope('/', function (RouteBuilder $routes) {
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
